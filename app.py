7eb355b9b186e4dd8c63811833cf474f12ce4bba
buriihenry <buriihenry@gmail.com>
2022-12-09 01:44:42 +0300

    Creating IMDb API object

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 from difflib import get_close_matches
 from tmdbv3api import TMDb, Movie
 import requests
+
+# Initialising flask app
+app = Flask(__name__)
+
+# load the data
+df = pd.read_csv('Data/preprocessed_data.csv')
+# load cache data
+df_cache = pd.read_csv('Data/cache_data.csv')
+# storing movie title into list
+movie_list = list(df['movie_title'])
+
+# creating TMDB Api Object
+tmdb = TMDb()
+tmdb.api_key = 'e955d66146c91573e52a09a5566459d4'
