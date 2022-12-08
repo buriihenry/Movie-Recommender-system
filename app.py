1cb0c58c06327c168c16ab69523e034c86f292a6
buriihenry <buriihenry@gmail.com>
2022-12-09 01:40:36 +0300

    Importing Libraries

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-#
+# Importing the necessary libraries
+from flask import Flask, request, render_template
+from flask_cors import cross_origin
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from difflib import get_close_matches
+from tmdbv3api import TMDb, Movie
+import requests
