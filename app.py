9af3f5358d7afcf4ccdff9a23faaff8a61d54461
buriihenry <buriihenry@gmail.com>
2022-12-09 07:05:04 +0300

    Home page

--- app.py
+++ app.py
@@ -35,3 +35,35 @@ def get_poster_link(title_list):
 
     for title in title_list:
         # checking given movie is present in our cache database or not.
+        r_df = df_cache[df_cache['Title'] == title]
+        try:
+            # if given movie is found in our cache database then run this part
+            if len(r_df) >= 1:
+                dic_data["Movie_Title"].append(r_df['Movie_Title'].values[0])
+                dic_data["Poster_Links"].append(r_df['Poster_Links'].values[0])
+                dic_data["Tag_Line"].append(r_df['Tag_Line'].values[0])
+
+            # otherwise retrieve the data from tmdbi api
+            else:
+                result = tmdb_movie.search(title)
+                movie_id = result[0].id
+                response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
+                data_json = response.json()
+
+                # Fetching movie title and poster link
+                movie_title = data_json['title']
+                movie_poster_link = "https://image.tmdb.org/t/p/original" + data_json['poster_path']
+                movie_tag_line = data_json['tagline']
+
+                # Appending movie title and poster link into dictionary
+                dic_data['Movie_Title'].append(movie_title)
+                dic_data['Poster_Links'].append(movie_poster_link)
+                dic_data['Tag_Line'].append(movie_tag_line)
+        except:
+            pass        
+    return dic_data
+@app.route('/', methods=['GET']) #route to display homepage
+@cross_origin()
+def home():
+    return render_template('index.html')
+     
